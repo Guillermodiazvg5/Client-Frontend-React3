900f6253774d7fcf281d18da36ca741338aea161
Guillermodiazvg5 <guillermodiazvg5@hotmail.com>
2024-01-07 13:07:43 -0500

    guardado6

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,106 +1,103 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
+import "./ShoppingCart.css";
 
-import './ShoppingCart.css'
+export default function ShoppingCart({
+  allProducts,
+  setAllProducts,
+  countProducts,
+  setCountProducts,
+  total,
+  setTotal,
+}) {
+  const onDeleteProduct = (producto) => {
+    const results = allProducts.filter((item) => item.id !== producto.id);
 
+    setTotal(total - producto.precio_pesos * producto.cantidad_carrito);
 
-export default function ShoppingCart({ allProducts , setAllProducts , countProducts , setCountProducts ,  total , setTotal }) {
+    setCountProducts(countProducts - producto.cantidad_carrito);
 
+    setAllProducts(results);
+  };
 
+  const onclearAllCart = () => {
+    setAllProducts([]);
+    setTotal(0);
+  };
 
-const onDeleteProduct= producto =>{
+  const PagarProductos = async (productos) => {
+    
+    if (allProducts.length > 0) {
+      const response = await axios.post(
+        "http://localhost:7000/create-order",
+        productos
+      );
 
-  const results = allProducts.filter(item => item.id !== producto.id)
-
-
-
-  setTotal(total - producto.precio_pesos * producto.cantidad_carrito)
-
-  setCountProducts(countProducts - producto.cantidad_carrito  );
-
-  setAllProducts(results)
-};
-
-
-
-const onclearAllCart = () => {
-
-   setAllProducts([]);
-   setTotal(0);
-   
-  
-
-
-} ;
-
-
-
-
-
-
-
+      window.location.href = response.data;
+    } else {
+      alert("Carrito Vacío, agrega productos para pagar");
+    }
+  };
 
   return (
-    
-    
-  <Table striped bordered hover className="mx-auto w-75   ">
+    <Table striped bordered hover className="mx-auto w-75   ">
       <thead>
         <tr>
-          
           <th>Cantidad</th>
           <th>Producto</th>
           <th>Precio</th>
         </tr>
       </thead>
 
-
-
       <tbody>
-
-         {allProducts.map(producto => (
-
-        <tr key={producto.id}>
-         
-          <td>{producto.cantidad_carrito}</td>
-          <td>{producto.producto}</td>
-          <td>{producto.precio_pesos}</td>
-          <td>  <Button onClick={() => onDeleteProduct(producto)} variant="outline-danger">X</Button>{' '} </td>
-
-        </tr>
-
-
-
-
-
-
-       ))}
+        {allProducts.map((producto) => (
+          <tr key={producto.id}>
+            <td>{producto.cantidad_carrito}</td>
+            <td>{producto.producto}</td>
+            <td>{producto.precio_pesos}</td>
+            <td>
+              {" "}
+              <Button
+                onClick={() => onDeleteProduct(producto)}
+                variant="outline-danger"
+              >
+                X
+              </Button>{" "}
+            </td>
+          </tr>
+        ))}
 
         <tr>
-          
-          <td colSpan={4}>  TOTAL : $ {total}    </td>
-
+          <td colSpan={4}> TOTAL : $ {total} </td>
         </tr>
-
 
         <tr>
-         
-        <td colSpan={3}> <div className="d-grid gap-2">  <Button className='mt-2 btn-pagar' variant="success"  size="lg">Pagar</Button>{' '} </div></td>
-        <td>  <Button onClick={onclearAllCart}  variant="outline-danger">Vaciar Carrito</Button>{' '} </td>
-          
+          <td colSpan={3}>
+            {" "}
+            <div className="d-grid gap-2">
+              {" "}
+              <Button
+                onClick={() => PagarProductos()}
+                className="mt-2 btn-pagar"
+                variant="success"
+                size="lg"
+              >
+                Pagar
+              </Button>{" "}
+            </div>
+          </td>
+          <td>
+            {" "}
+            <Button onClick={onclearAllCart} variant="outline-danger">
+              Vaciar Carrito
+            </Button>{" "}
+          </td>
         </tr>
-
-        
-      
       </tbody>
-      
-
-
     </Table>
-      
-    
-  )
-//console.log(allProducts);
-  
+  );
+  //console.log(allProducts);
 }
